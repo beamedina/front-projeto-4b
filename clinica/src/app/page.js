ad440dcdac22f339a0774095fba61cc10e16a61c
beamedina <alexandramedina@estudante.ifro.edu.br>
2025-02-12 14:27:39 -0400

    10/02

--- clinica/src/app/page.js
+++ clinica/src/app/page.js
@@ -1,12 +1,17 @@
 import styles from "./page.module.css";
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-    
+    <div className={styles.containerPrincipal}>
+      <h1>Clínica de Saúde M.O</h1>
+      <p>Boas-vindas ao site da nossa clínica! Aqui estarão disponíveis as informações sobre nossos médicos, pacientes e consultas.</p>
+      <div className={styles.divFotos}>
+        <Image className={styles.imgPage} src='/images/auscuta.jpg' alt='' width={500} height={300} />
+        <Image className={styles.imgPage} src='/images/anota.jpg' alt='' width={500} height={300} />
+      </div>
     </div>
   );
 }
-//  lloko
+//  400px pra cima responsivo
 
